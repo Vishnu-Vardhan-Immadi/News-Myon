01c116c4cd71a463cbbbb806220262273a3b0f50
Vishnu-Vardhan-Immadi <vishnuvardh9@gmail.com>
2023-10-13 11:05:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,66 @@ import Sidebar from './components/SideBar';
 import News from './components/News'
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+export default class App extends Component {
+  pageSize=24;
+  state={
+      progress:0,
+      articlesLoaded: false,
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
+  handleArticlesLoaded = () => {
+    this.setState({ articlesLoaded: true });
+  }
+  
+
+  render() {
+    return (
+      <div className='body'>
+        <Router>
+      <LoadingBar
+      height={3}
+        color='#f11946'
+        progress={this.state.progress}
+      />
+      
+      <Navbar/>
+      <Header /> 
+      <main className="main-content">
+        <Sidebar />
+        {/* <CategoryList />  */}
+      <Routes> 
+      {/* <Route path="/" element={<Sidebar />} /> */}
+      <Route
+      path="/News/:category"
+      element={<News setProgress={this.setProgress}  />}
+      />
+          <Route exact path="/category/general" element={<News setProgress={this.setProgress}  key="general" pageSize={this.pageSize} country="in" category="general"/> }></Route>
+         
+          <Route exact path="/category/business" element={<News setProgress={this.setProgress}  key="business" pageSize={this.pageSize} country="in" category="business"/> }></Route>
+          <Route exact path="/category/entertainment" element={<News setProgress={this.setProgress}  key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/> }></Route>
+          
+          <Route exact path="/category/health" element={<News setProgress={this.setProgress}  key="health" pageSize={this.pageSize} country="in" category="health"/> }></Route>
+          <Route exact path="/category/sports" element={<News setProgress={this.setProgress}  key="sports" pageSize={this.pageSize} country="in" category="sports"/> }></Route>
+          <Route exact path="/category/science" element={<News setProgress={this.setProgress}  key="science" pageSize={this.pageSize} country="in" category="science"/> }></Route>
+          <Route exact path="/category/technology" element={<News setProgress={this.setProgress}  key="technology" pageSize={this.pageSize} country="in" category="technology"/> }></Route>
+          
+          <Route path="/" element={ <News setProgress={this.setProgress}  pageSize={this.pageSize} country="in" category="general"/>}></Route>
+    </Routes>
+    </main>
+    {this.state.articlesLoaded && <Footer />} {/* Conditionally render the Footer */}
+        
+        </Router>  
+      </div>
+    )
+  }
 }
 
 
